193adb64891a921cefa27abd2354fff34cff2930
The-Infamaous-Spoon <garrett.kay01@student.csulb.edu>
2023-10-02 22:04:38 -0700

    p

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request
 
 views = Blueprint('views',__name__)
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def return_home():
     return render_template("index.html")
